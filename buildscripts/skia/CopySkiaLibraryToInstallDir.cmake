951df480b9194268708ceea61c75240d0d486588
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-08-05 09:36:04 +0200

    build: add target_link_directories because we're pulling a library from a file and not a target

--- buildscripts/skia/CopySkiaLibraryToInstallDir.cmake
+++ buildscripts/skia/CopySkiaLibraryToInstallDir.cmake
@@ -1,8 +1,10 @@
 message("-- Generated shared library")
 message("   from target Engine")
 message("   in ${SKIA_BUILD_DIR}/libskia.friction.so")
-message("-- Moving libskia.friction.so")
-message("   to ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR}")
 
+message("-- Moving libskia.friction.so")
+message("   to ${CMAKE_BINARY_DIR}/src")
+message("   so it is importable using \"\"")
 file(COPY ${SKIA_BUILD_DIR}/libskia.friction.so
-     DESTINATION ${CMAKE_BINARY_DIR}/${CMAKE_INSTALL_LIBDIR})
+     DESTINATION ${CMAKE_BINARY_DIR}/src)
+
