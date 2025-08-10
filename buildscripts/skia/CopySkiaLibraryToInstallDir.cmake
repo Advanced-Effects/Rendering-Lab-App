cc836c5262e55d4b41eb9a8042e090eb75443fa7
kaixoo12 <208606493+kaixoo12@users.noreply.github.com>
2025-08-10 15:58:14 +0200

    fix: CopySkiaLibraryInstallDir needed GNUInstallDirs

--- buildscripts/skia/CopySkiaLibraryToInstallDir.cmake
+++ buildscripts/skia/CopySkiaLibraryToInstallDir.cmake
@@ -5,6 +5,7 @@ message("   in ${SKIA_BUILD_DIR}/libskia.friction.so")
 message("-- Moving libskia.friction.so")
 message("   to ${CMAKE_BINARY_DIR}/src")
 message("   so it is importable using \"\"")
+include(GNUInstallDirs)
 install(
     FILES
     ${SKIA_BUILD_DIR}/libskia.friction.so
